5aab8daccac01b86a27bc0a799f929c74fb60677
kamrancode <kamranparmasii@gmail.com>
2021-10-05 22:00:38 +0330

    created posts folder and move markdown files into it, created blog and [slug] pages, Post and CategoryLabels components

---
+++ pages/blog/[slug].js
@@ -0,0 +1,72 @@
+/* eslint-disable @next/next/no-img-element */
+import fs from "fs";
+import matter from "gray-matter";
+import marked from "marked";
+import path from "path";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "../../components/Layout";
+import CategoryLabels from "../../components/CategoryLabels";
+
+const PostPage = ({
+  frontmatter: { title, category, date, cover_image, author, author_image },
+  content,
+  slug,
+}) => {
+  return (
+    <Layout title={title}>
+      <Link href="/blog">Go Back</Link>
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabels>{category}</CategoryLabels>
+        </div>
+        <img src={cover_image} alt={title} className="w-full rounded" />
+
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <img
+              src={author_image}
+              alt={title}
+              className="mx-4 w-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const files = fs.readdirSync(path.join("posts"));
+
+  const paths = files.map((filename) => ({
+    params: { slug: filename.replace(".md", "") },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const markdownWithMeta = fs.readFileSync(
+    path.join("posts", slug + ".md"),
+    "utf-8"
+  );
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: { frontmatter, content, slug },
+  };
+};
+
+export default PostPage;
