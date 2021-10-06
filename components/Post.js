b7f2f6ae8dd37971c008c6cdde1af4257c0309df
kamrancode <kamranparmasii@gmail.com>
2021-10-06 18:19:12 +0330

    Search, SearchResults and get search term from search file in api folder

--- components/Post.js
+++ components/Post.js
@@ -3,16 +3,17 @@ import Link from "next/link";
 import Image from "next/image";
 import CategoryLabels from "./CategoryLabels";
 
-const Post = ({ post }) => {
+const Post = ({ post, compact }) => {
   return (
     <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
-      <Image
-        src={post.frontmatter.cover_image}
-        alt={post.frontmatter.title}
-        height={420}
-        width={600}
-      />
-
+      {!compact && (
+        <Image
+          src={post.frontmatter.cover_image}
+          alt={post.frontmatter.title}
+          height={420}
+          width={600}
+        />
+      )}
       <div className="flex justify-between items-center">
         <span className="font-light text-gray-600">
           {post.frontmatter.date}
@@ -29,19 +30,23 @@ const Post = ({ post }) => {
         <p className="mt-2 text-gray-600">{post.frontmatter.excerpt}</p>
       </div>
 
-      <div className="flex justify-between items-center mt-6">
-        <Link href={`/blog/${post.slug}`}>
-          <a className="text-gray-600 hover:text-blue-600">Read More</a>
-        </Link>
-        <div className="flex items-center">
-          <img
-            src={post.frontmatter.author_image}
-            alt={post.frontmatter.title}
-            className="mx-4 object-cover rounded-full hidden sm:block"
-          />
-          <h3 className="text-gray-700 font-bold">{post.frontmatter.author}</h3>
+      {!compact && (
+        <div className="flex justify-between items-center mt-6">
+          <Link href={`/blog/${post.slug}`}>
+            <a className="text-gray-600 hover:text-blue-600">Read More</a>
+          </Link>
+          <div className="flex items-center">
+            <img
+              src={post.frontmatter.author_image}
+              alt={post.frontmatter.title}
+              className="mx-4 object-cover rounded-full hidden sm:block"
+            />
+            <h3 className="text-gray-700 font-bold">
+              {post.frontmatter.author}
+            </h3>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
